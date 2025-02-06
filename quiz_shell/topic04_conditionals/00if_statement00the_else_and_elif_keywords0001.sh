8f53930bab9785c3484c6f0ef0b1cc721a4a2dea
Marco Valsania Bacherer <98638679+mvalsania@users.noreply.github.com>
2025-02-06 00:11:35 -0800

    Update 00if_statement00the_else_and_elif_keywords0001.sh
    I believe this one might be a repeat

--- quiz_shell/topic04_conditionals/00if_statement00the_else_and_elif_keywords0001.sh
+++ quiz_shell/topic04_conditionals/00if_statement00the_else_and_elif_keywords0001.sh
@@ -1,14 +1,1 @@
-cd; rm -rf quiz; mkdir quiz; cd quiz
-foo='hola'
-cat > quiz.sh <<EOF
-foo='hello'
-if [ "$foo" = "hello" ]; then
-    touch if
-elif [ "$foo" = "hola" ]; then
-    touch elif
-else
-    touch else
-fi
-EOF
-sh quiz.sh
-ls
+
