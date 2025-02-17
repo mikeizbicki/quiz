6fad2b74a8e3d168f7b090878099f27b2b13c706
Mike Izbicki <mike@izbicki.me>
2025-02-17 11:01:26 -0800

    quiz

---
+++ quiz_shell/quiz_05_06b/01glob_0004.sh
@@ -0,0 +1,13 @@
+cd; rm -rf quiz; mkdir quiz; cd quiz
+git init
+mkdir test
+touch hola mundo
+touch test/'hello world'
+touch test/'.salve .munde'
+cd test
+for i in '*'; do git add $i; done
+git commit -m 'first commit'
+git checkout -b foo
+git add .
+git commit -m 'second commit'
+ls -a
