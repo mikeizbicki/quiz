6999b36267a7b3761312837661cc4035ca92b1be
Mike Izbicki <mike@izbicki.me>
2026-02-25 09:37:30 -0800

    update

--- quiz_python_with_exceptions/quiz05_exceptions/extra_0005f.py
+++ quiz_python_with_exceptions/quiz05_exceptions/extra_0005f.py
@@ -3,15 +3,11 @@ tweets = [
     { 'text': 'world', 'username': 'Obama' },
     { 'text': 'hola', 'username': 'Obama' },
     { 'text': 'mundo', 'name': 'Trump' },
-]
+    ]
 greetings = ['hello', 'world']
-
 num_greetings = 0
-for i in range(len(tweets)):
-    if tweets[i]['text'] in greetings:
-        num_greetings += 1
-
+for tweet in tweets:
+    if tweet['text'] in greetings:
+        if 'username' == 'Trump':
+            num_greetings += 1
 print('num_greetings=', num_greetings)
-
-
-
