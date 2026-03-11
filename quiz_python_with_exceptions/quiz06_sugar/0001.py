ef0116db06690e5116e5d1bcb029d4ed9eb49d73
Mike Izbicki <mike@izbicki.me>
2026-03-11 09:47:35 -0700

    quiz6

---
+++ quiz_python_with_exceptions/quiz06_sugar/0001.py
@@ -0,0 +1,5 @@
+a = 5
+b = 10
+for i in range(-2, -4, -1):
+    a, b = b, a
+print('a=', a)
---
+++ quiz_python_with_exceptions/quiz06_sugar/0002.py
@@ -0,0 +1,5 @@
+result = ''
+for a, b in zip('hola', 'mundo'):
+    result += a
+    result += b
+print('result=', result)
