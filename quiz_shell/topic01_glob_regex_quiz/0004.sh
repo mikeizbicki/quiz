4202a84d18b69d11974751ad2ade035ed3d40a04
Mike Izbicki <mike@izbicki.me>
2026-04-08 09:39:56 -0700

    regex quiz

---
+++ quiz_shell/topic01_glob_regex_quiz/0004.sh
@@ -0,0 +1,14 @@
+cd; rm -rf quiz; mkdir quiz; cd quiz
+mkdir romance
+mkdir germanic
+echo 'hello world'      > romance/README.en # english
+echo 'hola mundo'       > romance/README.es # spanish
+echo 'salve munde'      > romance/README.la # latin
+echo 'hallo welt'       > germanic/README.de # german
+echo 'hallo wereld'     > germanic/README.nl # dutch
+echo 'hej verden'       > germanic/README.da # danish
+echo 'hei verden'       > germanic/README.no # norwegian
+echo 'ola mundo'        > romance/README.pt # portuguese
+echo 'bonjour le monde' > romance/README.fr # french
+echo 'saluton mondo'    > romance/README.epo # esperanto
+cat */*e* | wc -l
