4799f2da033d77b9cb2cbcaa420c97c1cb60617d
Mike Izbicki <mike@izbicki.me>
2026-04-15 09:33:45 -0700

    quiz11

---
+++ quiz_python_with_exceptions/quiz11_config_formats/0001.py
@@ -0,0 +1,34 @@
+import yaml
+contents='''
+name: command_line
+on:
+  push:
+    branches:
+    - main
+    - master
+    - testing: true
+jobs:
+  tests:
+    strategy:
+      matrix:
+        python:
+        - 3.9
+        - 3.10
+        - 3.11
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - name: Set up Python ${{matrix.python}}
+        uses: actions/setup-python@v2
+        with:
+          python-version: ${{matrix.python}}
+      - name: Run tests
+        run:
+          pip3 install .
+          markdown-compiler --input_file=README.md
+          markdown-compiler --input_file=README.md --add_css
+'''
+data = yaml.safe_load(contents)
+
+x = len(data['jobs']['runs-on']['ubuntu-latest'])
+print('x=', x)
