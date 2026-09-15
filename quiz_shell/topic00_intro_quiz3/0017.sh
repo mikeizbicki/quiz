aeca9474498b81b80733fae65ac8fc80bb50962a
Mike Izbicki <mike@izbicki.me>
2026-09-15 08:54:46 -0700

    update

--- quiz_shell/topic00_intro_quiz3/0017.sh
+++ quiz_shell/topic00_intro_quiz3/0017.sh
@@ -1,14 +1,12 @@
 cd; rm -rf quiz; mkdir quiz; cd quiz
 git init
-mkdir master
-echo "print('hello world')" > master/foo.py
-echo "print('hola mundo')" > foo.py
-git add master
-git commit -m "added master"
+echo "print('hello world')" > foo.py
+git add foo.py
+git commit -m "added foo"
 git branch foo
 git checkout foo
 echo "print('hola mundo')" >> foo.py
 git add foo.py
 git commit -m "modified foo"
 git checkout master
-ls | wc -l
+python3 foo.py
